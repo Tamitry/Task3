1c5613bc43e7f08f4d4e35120587ceddc4f5d692
Tamitry <45293078+Tamitry@users.noreply.github.com>
2019-07-10 00:05:18 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
-import java.util.Random;
-
+/**
+ * @author Тарликовский Дмитрий
+ */
+//Уже засыпаю. Что смог подправить - подправил. Потом ещё.
 public class Main {
 
     public static void main(String[] args) {
-        Task34(1, 10000);
+        //headsandTails();
+
+        //maxNumeral();
+
+        //isPalindrome();   //Определяет, является ли число палиндромом. Ввод число, вывод в консоль и возвращает
+                            //boolean.
+        //isPrimeNumberConsole();   //Два варианта программы.
+                                    // Эта выводит в консоль, является ли введеное число n простым.
+        //primeDividers();  //Выводит все простые делители числа n.
+        //searchofLCSandGCD();  //Ищет и выводит НОК и НОД двух чисел a и b.
+        //numeralCount();   //Выводит количество различных цифр в числе n.
+        //isPerfectNumbers(); //Определяет, совершенно ли число n.
+        //amicableNumbers();    //Задается промежуток, в котором ищутся дружественные числа, от а до b.
     }
-    static void Task31() {
+    //Метод тысячу раз подбрасывает монету. Выводит информацию о результатах. Не требует параметров.
+    static void headsandTails() {
         int heads = 0;//Орёл и
         int tails = 0;//решка
         int count = 1000;
+
         for(int i=0;i<count;i++){
             if(Math.round(Math.random())==0){
                 heads++;
@@ -17,11 +33,15 @@ public class Main {
                 tails++;
             }
         }
+
         System.out.println("Количество выпадений орла: "+heads+", выпадений решки: "+tails);
     }
-    static void Task321() {
+    //Не совсем понятное задание. Насколько я понял, требовалось доказать, что 9 наибольшая цифра.
+    //Берется 0 и делиться на разряд. Как только остаток будет отличаться от числителя, значит число уже двуразрядное.
+    static void maxNumeral() {
         int n=0;
         int max = 0;
+
         while(n%10==n){
             if(n>max){
                 max=n;
@@ -30,74 +50,111 @@ public class Main {
         }
         System.out.println(max);
     }
-    static void Task322(int n) {
-        int temp=n;
+
+    //Определяет, является ли число палиндромом. Ввод число, вывод в консоль и возвращает
+    //boolean.
+    static boolean isPalindrome(int number) {
+        int temp=number;
         int length=0;
         int i=0;
-        int potomNazovu=1;
-        while (temp!=0){
+        int digitNumber=1;
+
+        while (temp!=0){//количество разрядов, чтобы можно было отделять цифры
             temp/=10;
             length++;
-            potomNazovu*=10;
+            digitNumber*=10;
         }
-        potomNazovu/=10;
+
+        digitNumber/=10;
+
         while (i<length/2){
-            if(n/potomNazovu!=n%10){
+
+            if(number/digitNumber!=number%10){
                 System.out.println("Не верно.");
-                return;
+                return false;
             }
-            n%=potomNazovu;
-            potomNazovu/=100;
-            n/=10;
+
+            number%=digitNumber;
+            digitNumber/=100;//забираем спереди и сзади
+            number/=10;
             i++;
+
         }
         System.out.println("Верно.");
+        return true;
     }
-    static boolean Task323(int n) {
-        for(int i=2;i<=Math.sqrt(n);i++) {
-            if(n%i == 0) {
+
+    // Эта выводит в консоль, является ли введеное число number простым.
+    static boolean isPrimeNumberBoolean(int number) {
+
+        for(int i=2;i<=Math.sqrt(number);i++) {
+            if(number%i == 0) {
                 return false;
             }
         }
+
         return true;
-    }//Для проверки этого задания используйте Task324. Если введете простое число, оно
-                                       //сообщит об этом, а если обычное, то покажет простые делители.
-    static void Task324(int n) {
-        if(n<=3) {
+    }
+
+    //Выводит все простые делители числа number.
+    static void isPrimeNumberConsole(int number) {
+        if(isPrimeNumberBoolean(number)) {
+            System.out.println("Простое.");
+        }
+        else {
+            System.out.println("Не простое.");
+        }
+    }
+
+    //Выводит все простые делители числа number.
+    static void primeDividers(int number) {
+
+        if(number <=3) {
             System.out.println("Введите больше 3, так как 2 и 3 простые числа.");
             return;
         }
-        if(Task323(n)) {
+
+        if(isPrimeNumberBoolean(number)) {
             System.out.println("Число простое.");
             return;
         }
-        String line = "2";
-        for(int i=3;i<=n/2;i++) {
-            if(n%10==0) {
-                if (Task323(i)) {
-                    line = line + ", " + i;
+
+        String line = "";
+        for(int i = 2; i<= number /2; i++) {
+            if(number %i==0) {
+                if (isPrimeNumberBoolean(i)) {
+                    line = line  + i + ", ";
                 }
             }
         }
+
         System.out.println(line);
     }
-    static void Task325(int a, int b) {
-        int cm = a*b; //CM - common multiple
-        while (a!=0 && b!=0) {
-            if(a>b) {
-                a = a%b;
+
+    //Ищет и выводит НОК и НОД двух чисел number1 и number2.
+    static void searchofLCSandGCD(int number1, int number2) {
+        int cm = number1 *number2; //CM - common multiple
+
+        while (number1 !=0 && number2!=0) {
+            if(number1 >number2) {
+                number1 = number1 %number2;
             }
             else {
-                b = b%a;
+                number2 = number2% number1;
             }
         }
-        System.out.println("НОД: "+(a+b));
-        System.out.println("НОК:"+(cm/(a+b)));
+
+        System.out.println("НОД: "+(number1 +number2));
+        System.out.println("НОК:"+(cm/(number1 +number2)));
     }
-    static void Task326(int n) {
+
+    //Выводит количество различных цифр в числе number.
+    static void numeralCount(int number) {
+
         for (int i = 0; i<=9;i++) {
-            int temp = n;
+            int temp = number;
             int count = 0;
+
             while (temp!=0) {
                 int numeral = temp%10;
                 temp/=10;
@@ -105,51 +162,65 @@ public class Main {
                     count++;
                 }
             }
+
             System.out.println("Цифра "+i+" появилась "+count+" раз.");
         }
     }
-    static void Task33(int n) {
+
+    //Определяет, совершенно ли число number.
+    static boolean isPerfectNumbers(int number) {
         int temp = 0;
-        for(int i=1;i<=n/2;i++) {
-            if(n%i==0) {
+
+        for(int i=1;i<=number/2;i++) {
+            if(number%i==0) {
                 temp+=i;
             }
         }
-        if(temp==n) {
+        if(temp==number) {
             System.out.println("Число совершенное.");
+            return true;
         }
         else {
             System.out.println("Несовершенное.");
+            return false;
         }
     }
-    static void Task34(int a, int b) {
-        for(int i=a;i<b;i++) {
-            for (int j=i+1;j<=b;j++) {
+
+    //Задается промежуток, в котором ищутся дружественные числа, от а до rightBorder.
+    static void amicableNumbers(int leftBorder, int rightBorder) {
+        for(int i = leftBorder; i<rightBorder; i++) {
+            for (int j=i+1;j<=rightBorder;j++) {
+
                 int temp1 = 0;
                 int temp2 = 0;
+
                 for(int k=1;k<=i/2;k++) {
                     if(i%k==0) {
                         temp1+=k;
                     }
                 }
+
                 for(int k=1;k<=j/2;k++) {
                     if(j%k==0) {
                         temp2+=k;
                     }
                 }
+
                 if(temp1==j && temp2==i) {
                     System.out.println("Дружественные числа "+temp1+" и "+temp2);
                 }
             }
         }
-    }//Долгий и кривой способ "В ЛОБ". 2 пары за 3 минуты))))
-                                            // 13 минут для чисел от 1 до 10000. Как выяснилось, другого способа нет.
+    }//Долгий и кривой способ "В ЛОБ". 2 пары за 3 минуты
+                                            // 13 минут для чисел от 1 до 10000.
+                                            //Более оптимизированного варианта придумать не смог
+
     /*static void Task341(int a, int b) {
         for(int i=a;i<=b;i++){
             int p = 3*(int)Math.pow(2,(double)i-1)-1;
             int q = 3*(int)Math.pow(2,(double)i)-1;
             int r = 9*(int)Math.pow(2,2*i-1)-1;
-            if(Task323(p)&&Task323(q)&&Task323(r)) {
+            if(isPrimeNumberBoolean(p)&&isPrimeNumberBoolean(q)&&isPrimeNumberBoolean(r)) {
                 System.out.println("Дружественные числа "+(int)Math.pow(2,i)*p*q+" и "+(int)Math.pow(2,i)*r);
             }
         }
